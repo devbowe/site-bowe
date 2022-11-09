dc438b3d3bbeb251ea87cb7e46d84fabdd986f75
Felipe Costa <felipestscosta@gmail.com>
2022-11-09 12:16:04 -0300

    Removendo Header COR

--- src/services/contact.js
+++ src/services/contact.js
@@ -3,13 +3,8 @@ import http from "./http"
 const send = params => {
   return new Promise((resolve, reject) => {
     const endpoint = "contact/send"
-
-    const headers = {
-      "access-control-allow-origin": "*",
-    }
-
     http
-      .post(endpoint, params, headers)
+      .post(endpoint, params)
       .then(response => resolve(response))
       .catch(error => reject(error))
   })
