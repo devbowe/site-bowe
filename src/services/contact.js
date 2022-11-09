04851caeab157ccb52dccdde53d2afabf5f911f8
Felipe Costa <felipestscosta@gmail.com>
2022-11-09 11:12:34 -0300

    Adicionando cabeçalho permitido a requisição

--- src/services/contact.js
+++ src/services/contact.js
@@ -4,8 +4,12 @@ const send = params => {
   return new Promise((resolve, reject) => {
     const endpoint = "contact/send"
 
+    const headers = {
+      "access-control-allow-origin": "*",
+    }
+
     http
-      .post(endpoint, params)
+      .post(endpoint, params, headers)
       .then(response => resolve(response))
       .catch(error => reject(error))
   })
